7c8ddbea1102ddcc9f63d08509b66f06c3b6026a
Prakhar Kumar Chandraker <prakharkumarchandraker@gmail.com>
2023-03-11 22:31:22 +0530

    100 days of code in python by CodeWithHarry

--- Day 14/Day14 myif.py
+++ Day 14/Day14 myif.py
@@ -2,10 +2,10 @@ a = int(input("Enter your age: "))
 print("Your age is:", a)
 # Conditional operators
 # >, <, >=, <=, ==, !=
-# print(a>18)
-# print(a<=18)
-# print(a==18)
-# print(a!=18)
+print(a>18)
+print(a<=18)
+print(a==18)
+print(a!=18)
 if(a>18):
     print("You can drive")
     print("Yes")
